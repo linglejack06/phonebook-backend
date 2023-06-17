45576280ce04c323a153c650b53b06173baf1c30
Jack Lingle <captainjack0818@gmail.com>
2023-06-17 10:19:35 -0400

    add morgan middleware

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const morgan = require('morgan');
 
 const app = express();
+// eslint-disable-next-line no-unused-vars
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 let persons = [
   {
     id: 1,
